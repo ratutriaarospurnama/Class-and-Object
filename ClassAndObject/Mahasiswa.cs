4a17ca832c2dcf487e62c84e1de676841b2445d3
ratutriaarospurnama <ratutriaarospurnama19@gmail.com>
2019-03-19 12:07:52 +0700

    Class And Object

---
+++ ClassAndObject/Mahasiswa.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndObject
+{
+    public class Mahasiswa
+    {
+        
+        public string Nim { get; set; }
+        public string Nama { get; set; }
+        public float Ipk { get; set; }
+        
+
+        public void Registrasi()
+        {
+            Console.WriteLine("Nama: {0}", Nama);
+            Console.WriteLine("Nim: {0}", Nim);
+            Console.WriteLine("IPK: {0}", Ipk);
+            Console.WriteLine("Telah melakukan registrasi");
+        }
+
+        public void IsiKrs()
+        {
+            Console.WriteLine("{0} sedang melakukan proses Krs\n", Nama);
+        }
+    }
+}
---
+++ ClassAndObject/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mahasiswa saya = new Mahasiswa();
+            Mahasiswa kamu = new Mahasiswa();
+
+            saya.Nim = "18.11.2336";
+            saya.Nama = "Ratu";
+            saya.Ipk = 4.0f;
+
+            kamu.Nim = "18.11.2318";
+            kamu.Nama = "Billa";
+            kamu.Ipk = 4.0f;
+
+            saya.Registrasi();
+            saya.IsiKrs();
+
+            kamu.Registrasi();
+            kamu.IsiKrs();
+
+            Console.ReadKey();
+        }
+    }
+}
